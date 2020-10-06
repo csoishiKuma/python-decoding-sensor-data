c9743220df826215f62552cd1eceb519d2b33a30
Craig Oishi <oisc@chevron.com>
2020-10-06 16:09:52 -0500

    just started

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -2,6 +2,7 @@ import os
 import glob
 import csv
 
-load_sensor_data()
-  sensor_data = []
+def load_sensor_data():
+    sensor_data = []
+    sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
   
