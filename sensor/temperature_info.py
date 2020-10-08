9e20da3070ddb308b81daf169e7ca0edc640f4e8
Craig Oishi <oisc@chevron.com>
2020-10-08 13:45:58 -0500

    Completed module

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -3,16 +3,16 @@ from datetime import date, datetime
 
 class TemperatureData(HouseInfo):
 
-    def __convert_data(self, data):
+    def _convert_data(self, data):
         recs = []
         for rec in data:
             recs.append(int(rec['temperature'], base=10))
         return recs
 
     def get_data_by_area(self, rec_area=0):
-        recs = super.get_data_by_area("temperature", rec_area)
-        return __convert_data(recs)
+        recs = super().get_data_by_area("temperature", rec_area)
+        return self._convert_data(recs)
 
     def get_data_by_date(self, rec_date=date.today()):
-        recs = super.get_data_by_date("temperature", rec_date)
-        return __convert_data(recs)
+        recs = super().get_data_by_date("temperature", rec_date)
+        return self._convert_data(recs)
